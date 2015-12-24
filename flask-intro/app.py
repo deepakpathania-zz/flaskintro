52ca3b60a3d38d07633bbd09896eef78ce4475e3
deepakpathania <deepakpathaniadp0@gmail.com>
2015-12-24 13:03:12 +0530

    rendering single template

---
+++ flask-intro/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+	return "Hey World!!!"
+
+@app.route('/welcome')
+def welcome():
+	return render_template("welcome.html")
+
+
+if __name__ == '__main__':
+	app.run()
