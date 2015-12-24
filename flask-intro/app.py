b862ed64ada30eb3d720115980e352d2836d6f1a
deepakpathania <deepakpathaniadp0@gmail.com>
2015-12-24 22:31:11 +0530

    database connectivity added

--- flask-intro/app.py
+++ flask-intro/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template,redirect,url_for,request,session,flash
+from flask import Flask, render_template,redirect,url_for,request,session,flash,g
 from functools import wraps
+import sqlite3
+
 app=Flask(__name__)
 
 app.secret_key="justsomerandomstuffyouknow"
-
+app.database = "sample.db"
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
@@ -19,7 +21,11 @@ def login_required(f):
 @login_required
 def home():
 	#return "Hey World!!!"
-	return render_template("index.html")
+	g.db = connect_db()
+	cur=g.db.execute('select * from posts')
+	posts=[dict(title=row[0],description = row[1]) for row in cur.fetchall()]
+	g.db.close()
+	return render_template("index.html",posts=posts)
 
 @app.route('/welcome')
 def welcome():
@@ -44,6 +50,8 @@ def logout() :
 	flash("You logged out !")
 	return redirect(url_for('welcome'))
 
+def connect_db():
+	return sqlite3.connect(app.database)
 
 if __name__ == '__main__':
 	app.run(debug=True)
